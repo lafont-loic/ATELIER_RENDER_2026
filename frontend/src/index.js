e252d52f7ac89654be75933a1b2ff79e57ba8db2
lafont-loic <loic.lafont.isr@gmail.com>
2026-04-01 17:30:07 +0200

    Create index.js

---
+++ frontend/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <h1 style={{ textAlign: 'center', marginTop: '50px' }}>
+      🚀 Frontend React Déployé sur Render !
+    </h1>
+  </React.StrictMode>
+);
